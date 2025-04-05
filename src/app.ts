a3dcebe379778b29efbf3756a947af1b04f200b6
mohit-bhandari45 <mohitbhandari852@gmail.com>
2025-04-05 11:50:14 +0530

    change the TTL to in db score field

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,28 @@
-import cors from "cors";
-import dotenv from "dotenv";
 import express, { Express, Request, Response } from "express";
-import { connectDB } from "./db.js";
+import cors from "cors";
 import apiRoutes from "./routes/api/index.js";
 import authRoutes from "./routes/auth/auth.js";
+import http from "http";
+import { Server } from "socket.io";
+import { fileURLToPath } from "url";
+import path from "path";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
+import dotenv from "dotenv";
+import { connectDB } from "./db.js";
 dotenv.config();
 connectDB();
 
 const app: Express = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+io.on("connection", (socket) => {
+    socket.on("user-message", (message) => {
+        io.emit("message", message);
+    });
+});
 
 /* Middlewares */
 app.use(express.json());
@@ -17,6 +31,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use("/auth", authRoutes);
 app.use("/api", apiRoutes);
+app.use(express.static(path.join(__dirname, "../public")));
 
 app.get("/up", (req: Request, res: Response) => {
     res.status(200).json({
@@ -24,4 +39,4 @@ app.get("/up", (req: Request, res: Response) => {
     });
 });
 
-export default app;
+export default server;
--- src/controllers/discover.ts
+++ src/controllers/discover.ts
@@ -1,12 +1,7 @@
 import { Request, Response } from "express";
 import Post from "../models/post.js";
-import { IResponse } from "./auth.js";
-import { IStats } from "../types/stats.js";
 import { setErrorDetails } from "../utils/helper.js";
-
-let trendingCache: any = null;
-let cachedTimeStamp: any = 0;
-const TLE: number = 5 * 60 * 1000;
+import { IResponse } from "../types/response.js";
 
 /**
  * @route GET /api/discover/trending
@@ -14,62 +9,17 @@ const TLE: number = 5 * 60 * 1000;
  * */
 async function getTrendingForteHandler(req: Request, res: Response): Promise<any> {
     try {
-        const now = Date.now();
+        const trendingFortes = await Post.aggregate([
+            {
+                $group: {
+                    _id: "$forte",
+                    totalScore: { $sum: "$score" },
+                    postCount: { $sum: 1 },
+                },
+            },
+            { $sort: { totalScore: -1 } },
+        ]);
 
-        if (trendingCache && now - cachedTimeStamp < TLE) {
-            console.log(now - cachedTimeStamp);
-            return res.status(200).json(trendingCache);
-        }
-
-        console.log("Mohit");
-
-        const currentDate = new Date();
-        const posts = await Post.find();
-        const forteStats: Record<string, IStats> = {};
-
-        posts.forEach((post) => {
-            const forte = post.forte;
-            if (!forteStats[forte]) {
-                forteStats[forte] = {
-                    postCount: 0,
-                    totalApplauds: 0,
-                    totalComments: 0,
-                    totalReplies: 0,
-                    recentPostCount: 0,
-                    score: 0,
-                };
-            }
-
-            const applauds = post.applauds.length;
-            const comments = post.comments.length;
-            const replies = post.comments.reduce((total, comment) => total + (comment.replies?.length || 0), 0);
-
-            const hoursSinceCreation = (currentDate.getTime() - post.createdAt.getTime()) / 36e5;
-            const isRecent = hoursSinceCreation < 24;
-
-            forteStats[forte].postCount += 1;
-            forteStats[forte].totalApplauds += applauds;
-            forteStats[forte].totalComments += comments;
-            forteStats[forte].totalReplies += replies;
-            if (isRecent) forteStats[forte].recentPostCount += 1;
-        });
-
-        /* Calculating Score */
-        Object.keys(forteStats).forEach((forte) => {
-            const data = forteStats[forte];
-            data.score = data.totalApplauds * 3 + data.totalComments * 2 + data.totalReplies + data.recentPostCount * 5;
-        });
-
-        /* Sorting */
-        const trendingFortes = Object.entries(forteStats)
-            .sort(([, a], [, b]) => b.score - a.score)
-            .map(([forte, data]) => ({
-                forte,
-                ...data,
-            }));
-
-        trendingCache = trendingFortes;
-        cachedTimeStamp = now;
         return res.status(200).json(trendingFortes);
     } catch (error) {
         return res.status(500).json(setErrorDetails("Internal Server Error", error as string));
@@ -99,8 +49,6 @@ async function discoverHandler(req: Request, res: Response): Promise<any> {
         const posts = await Post.find(match, { score: { $meta: "textScore" } })
             .sort({ score: { $meta: "textScore" }, createdAt: -1 })
             .limit(limit);
-
-        console.log(posts.length);
 
         response.posts = posts;
         res.status(200).json(response);
