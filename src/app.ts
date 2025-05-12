ecfae566f184d4592b2b9aa8a50b582d67470c56
mohit-bhandari45 <mohitbhandari852@gmail.com>
2025-05-12 11:44:54 +0530

    feat: json change

--- src/app.ts
+++ src/app.ts
@@ -38,8 +38,8 @@ io.on("connection", socketHandler(io));
 
 /* Middlewares */
 app.use(cors());
-app.use(express.json({ limit: "40mb" }));
-app.use(express.urlencoded({ extended: true, limit: "40mb" }));
+app.use(express.json({ limit: "200mb" }));
+app.use(express.urlencoded({ extended: true, limit: "200mb" }));
 
 app.use("/auth", authRoutes);
 app.use("/api", apiRoutes);
